43c31daecb0ac936f695ad2c546f38b003a02336
Abagail Hudson <abagailanne@gmail.com>
2019-05-03 01:45:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(speak)
-  if speak = "I LOVE YOU GRANDMA!"
+  if speak == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif speak = speak.uppercase
+  elsif speak == speak.uppercase
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
